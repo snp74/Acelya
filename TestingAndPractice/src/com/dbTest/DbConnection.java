6faa369200587adfb27d40f064ab37e9fd517537
Sachin <Sachin@192.168.1.100>
2018-12-05 06:51:14 -0800

    s

---
+++ TestingAndPractice/src/com/dbTest/DbConnection.java
@@ -0,0 +1,35 @@
+package com.dbTest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DbConnection {
+
+	public static void main(String[] args) {
+		try {
+			// step1 load the driver class
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			// step2 create the connection object
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "sachin", "sachin");
+
+			// step3 create the statement object
+			Statement stmt = con.createStatement();
+
+			// step4 execute query
+			ResultSet rs = stmt.executeQuery("select * from country");
+			while (rs.next())
+				System.out.println( rs.getString(1));
+
+			// step5 close the connection object
+			con.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
